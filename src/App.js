fb011413b39749e1aaa19c47886fa24172cdb96d
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-08-22 16:59:09 +0100

    Super styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Card from "./Stretch";
 function App() {
   return (
     <div className="App">
+      <h1>Daily Dawg</h1>
+      <p>
+        A quick stretch to keep ya limber. Refresh the page to view another!
+      </p>
       <Card />
     </div>
   );
